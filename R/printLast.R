cf6d2af72ae42ce0ed72acec42d19592d2d42282
Michael Pinus <pinusm@post.bgu.ac.il>
2019-04-29 21:33:20 +0300

    comments cleanup

--- R/printLast.R
+++ R/printLast.R
@@ -1,18 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
 #' print the last line, again, for use with keyboard shortcut
 #'
 #' @return none. a print method is used instead
